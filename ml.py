972077f09e7634c8039ee5138183e3eac951e693
diegobatt <diego.battocchio@gmail.com>
2019-04-18 17:05:34 -0300

    figuritas

--- ml.py
+++ ml.py
@@ -1,12 +1,14 @@
 import numpy as np
 import numpy.random
 
-from sklearn.mixture import GaussianMixture
+from sklearn.mixture import GaussianMixture, BayesianGaussianMixture
 import scipy.stats as ss
 import matplotlib.pyplot as plt
 
 
-n = 10000
+plt.style.use('ggplot')
+
+n = 100000
 mu = np.array([1, 4, 7])
 sigma = np.array([1, 0.5, 1])
 K = mu.shape[0]
@@ -16,23 +18,34 @@ mixture_idx = numpy.random.choice(
 samples = numpy.fromiter(
     (ss.norm.rvs(mu[i], sigma[i]) for i in mixture_idx),
     dtype=np.float64)
-samples = np.append(samples,[6]* 30)
+samples = np.append(samples,[12.5]* 500)
 
-x_axis = np.linspace(samples.min(), samples.max(), 200)
+x_axis = np.linspace(samples.min(), samples.max(), 400)
 y_axis = np.zeros_like(x_axis)
 
 for l, s, w in zip(mu, sigma, weights):
     y_axis += ss.norm.pdf(x_axis, loc=l, scale=s) * w
 
 y_axis_gmm = np.zeros_like(x_axis)
-gmm = GaussianMixture(5)
+gmm = GaussianMixture(7)
 gmm.fit(samples.reshape(-1,1))
 for l, s, w in zip(gmm.means_.flatten(), gmm.covariances_.flatten(), gmm.weights_):
     y_axis_gmm += ss.norm.pdf(x_axis, loc=l, scale=s) * w
 
-plt.plot(x_axis, y_axis)
-plt.plot(x_axis, y_axis_gmm)
-plt.hist(samples, normed=True, bins="fd")
-plt.xlabel("x")
-plt.ylabel("f(x)")
+y_axis_bgmm = np.zeros_like(x_axis)
+bgmm = BayesianGaussianMixture(7)
+bgmm.fit(samples.reshape(-1,1))
+for l, s, w in zip(bgmm.means_.flatten(), bgmm.covariances_.flatten(), bgmm.weights_):
+    y_axis_bgmm += ss.norm.pdf(x_axis, loc=l, scale=s) * w
+
+plt.plot(x_axis, y_axis, label='Densidad real')
+plt.hist(samples, normed=True, bins="fd", label='Muestras')
+# plt.plot(x_axis, y_axis_gmm, label='Estimacion ML')
+plt.plot(x_axis, y_axis_bgmm, label='Estimacion bayesiana')
+# plt.xlabel("x")
+# plt.ylabel("f(x)")
+# plt.xlim([-2, 10])
+# plt.ylim([0, 0.43])
+plt.legend()
+plt.savefig('figures/bayes_soluciones')
 plt.show()
