07a7b62f0a2c0d431c8df4b6089a840c922be7ac
diegobatt <diego.battocchio@gmail.com>
2019-04-26 23:36:12 -0300

    fixed images

--- ml.py
+++ ml.py
@@ -6,9 +6,9 @@ import scipy.stats as ss
 import matplotlib.pyplot as plt
 
 
-plt.style.use('ggplot')
+plt.style.use('seaborn')
 
-n = 100000
+n = 1000
 mu = np.array([1, 4, 7])
 sigma = np.array([1, 0.5, 1])
 K = mu.shape[0]
@@ -18,7 +18,7 @@ mixture_idx = numpy.random.choice(
 samples = numpy.fromiter(
     (ss.norm.rvs(mu[i], sigma[i]) for i in mixture_idx),
     dtype=np.float64)
-samples = np.append(samples,[12.5]* 500)
+samples = np.append(samples,[12.5]* int(n/200))
 
 x_axis = np.linspace(samples.min(), samples.max(), 400)
 y_axis = np.zeros_like(x_axis)
@@ -26,27 +26,31 @@ y_axis = np.zeros_like(x_axis)
 for l, s, w in zip(mu, sigma, weights):
     y_axis += ss.norm.pdf(x_axis, loc=l, scale=s) * w
 
+K = 15
+
 y_axis_gmm = np.zeros_like(x_axis)
-gmm = GaussianMixture(7)
+gmm = GaussianMixture(K)
 gmm.fit(samples.reshape(-1,1))
 for l, s, w in zip(gmm.means_.flatten(), gmm.covariances_.flatten(), gmm.weights_):
-    y_axis_gmm += ss.norm.pdf(x_axis, loc=l, scale=s) * w
+    y_axis_gmm += ss.norm.pdf(x_axis, loc=l, scale=np.sqrt(s)) * w
 
 y_axis_bgmm = np.zeros_like(x_axis)
-bgmm = BayesianGaussianMixture(7)
+bgmm = BayesianGaussianMixture(K)
 bgmm.fit(samples.reshape(-1,1))
 y_axis_bgmm = np.exp(bgmm.score_samples(x_axis.reshape(-1,1)))
 # for l, s, w in zip(bgmm.means_.flatten(), bgmm.covariances_.flatten(), bgmm.weights_):
 #     y_axis_bgmm += ss.norm.pdf(x_axis, loc=l, scale=s) * w
 
 plt.plot(x_axis, y_axis, label='Densidad real')
-plt.hist(samples, normed=True, bins="fd", label='Muestras')
+plt.hist(samples, normed=True, bins="fd", alpha=1, label='Muestras')
+# plt.plot(samples,np.zeros(len(samples)), marker='.', label='Muestras')
 # plt.plot(x_axis, y_axis_gmm, label='Estimacion ML')
 plt.plot(x_axis, y_axis_bgmm, label='Estimacion bayesiana')
 # plt.xlabel("x")
 # plt.ylabel("f(x)")
 # plt.xlim([-2, 10])
-# plt.ylim([0, 0.43])
+plt.ylim([0, 0.32])
 plt.legend()
 plt.savefig('figures/bayes_soluciones')
+# plt.savefig('figures/ml_problemas')
 plt.show()
